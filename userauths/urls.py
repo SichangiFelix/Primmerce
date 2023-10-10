68373a68a5836b06b1e28a0061b9238699063c27
SichangiFelix <sichangifelix23@gmail.com>
2023-10-10 10:42:56 +0300

    Created a signup form

--- userauths/urls.py
+++ userauths/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from userauths import views
 
-# app_name = "userauths"
+app_name = "userauths"
 
 urlpatterns = [
     path('sign-up/', views.register_view, name="sign-up"),
