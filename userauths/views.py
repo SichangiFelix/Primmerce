8054a61c52a6268c1a0e114154f4d15745a702e9
SichangiFelix <sichangifelix23@gmail.com>
2023-10-10 10:42:43 +0300

    Added login functionality after signup

--- userauths/views.py
+++ userauths/views.py
@@ -1,15 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import UserRegisterForm
 from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
 # Create your views here.
 @csrf_protect
 def register_view(request):
 
-    if request.method == "Post":
+    if request.method == "POST":
         form = UserRegisterForm(request.POST or None)
         if form.is_valid():
-            form.save()
+            new_user = form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Hey {username}, your account was created successfuly")
+            new_user = authenticate(username = form.cleaned_data['email'], password = form.cleaned_data['password1'])
+            login(request, new_user)
+            return redirect("core:index")
         
     else:
         form = UserRegisterForm()
