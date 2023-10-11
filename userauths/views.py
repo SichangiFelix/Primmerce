001909ea6059128ced729eabf70431d433a6e2c8
SichangiFelix <sichangifelix23@gmail.com>
2023-10-12 00:06:35 +0300

    Finish up auth

--- userauths/views.py
+++ userauths/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import redirect, render
 from .forms import UserRegisterForm
 from django.views.decorators.csrf import csrf_protect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.conf import settings
-
-User = settings.AUTH_USER_MODEL
+from userauths.models import User
 
 # Create your views here.
 @csrf_protect
@@ -39,20 +38,24 @@ def login_view(request):
 
         try:
             user = User.objects.get(email = email)
+            user = authenticate(request, email = email, password = password)
+            if user:
+                login(request, user)
+                messages.success(request, f"Successfuly logged in!")
+                return redirect("core:index")
+            else:
+                messages.warning(request, "User does not exist, create an account")
         except:
             messages.warning(request, f"User with {email} does not exist!")
-        
-        user = authenticate(request, email = email, password = password)
-
-        if user:
-            login(request, user)
-            messages.success(request, f"Successfuly logged in!")
-            return redirect("core:index")
-        else:
-            messages.warning(request, "User does not exist, create an account")
 
     context = {
 
     }
 
     return render(request, "userauths/sign-in.html", context)
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "You logged out")
+    return redirect("userauths:sign-in")
