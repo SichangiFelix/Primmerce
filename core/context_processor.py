d5cdbf91c56ff86fe7342432251e624911c7f6a1
SichangiFelix <sichangifelix23@gmail.com>
2023-10-24 15:11:33 +0300

    min max price

--- core/context_processor.py
+++ core/context_processor.py
@@ -1,8 +1,12 @@
 from core.models import Product, Category, Vendor, CartOrder, CartOrderItems, ProductImages, ProductReview, Wishlist, Address
+from django.db.models import Min, Max
 
 def default(request):
     categories = Category.objects.all()
     vendors = Vendor.objects.all()
+
+    min_max_price = Product.objects.aggregate(Min("price"), Max("price"))
+
     try:
         # address = Address.objects.get(user = request.user)
         pass
@@ -11,6 +15,7 @@ def default(request):
     return {
         'categories': categories, 
         'vendors': vendors,
+        'min_max_price': min_max_price,
         # 'address': address,
     }
 
