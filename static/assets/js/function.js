3ea6c10bb791b1876f2ff56c034d1e105aa48f75
SichangiFelix <sichangifelix23@gmail.com>
2023-10-24 15:12:06 +0300

    New filter logic

--- static/assets/js/function.js
+++ static/assets/js/function.js
@@ -57,10 +57,16 @@ $("#commentForm").submit(function(e){
 })
 
 $(document).ready(function (){
-    $(".filter-checkbox").on("click", function(){
+    $(".filter-checkbox, #price-filter-btn").on("click", function(){
         console.log("Checked out mahn!")
 
         let filter_object = {}
+
+        let min_price = $("#max_price").attr("min")
+        let max_price = $("#max_price").val()
+
+        filter_object.min_price = min_price
+        filter_object.max_price = max_price
 
         $(".filter-checkbox").each(function(){
             let filter_value = $(this).val()
@@ -75,6 +81,46 @@ $(document).ready(function (){
         })
 
         console.log("Filter Object is: ", filter_object);
+        $.ajax({
+            url: '/filter-products',
+            data: filter_object,
+            dataType: 'json',
+            beforeSend: function(){
+                console.log("Trying to filter products...");
+            },
+            success: function(response){
+                console.log(response.length);
+                console.log("Data filtered successfuly...");
+                $("#filtered-product").html(response.data)
+            }
+        })
+    })
 
+    $("#max_price").on("blur", function(){
+        let min_price = $(this).attr("min")
+        let max_price = $(this).attr("max")
+        let current_price = $(this).val()
+
+        // console.log("Current Price is:", current_price);
+        // console.log("Max Price is:", max_price);
+        // console.log("Min Price is:", min_price);
+
+        if(current_price < parseInt(min_price) || current_price > parseInt(max_price)){
+            console.log("Price Error Occured");
+
+            min_price = Math.round(min_price * 100)/100
+            max_price = Math.round(max_price * 100)/100
+
+            // console.log("################################################")
+            // console.log("Max Price is:", max_Price);
+            // console.log("Min Price is:", min_Price);
+            alert("Price must be between $ " + min_price + " and $" + max_price)
+            $(this).val(min_price)
+            $('#range').val(min_price)
+
+            $(this).focus()
+
+            return false
+        }
     })
 })
