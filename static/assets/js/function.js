fe400803ae8079552172d89350d4b5d7c8f40bff
SichangiFelix <sichangifelix23@gmail.com>
2023-10-27 12:31:31 +0300

    Implementing cart

--- static/assets/js/function.js
+++ static/assets/js/function.js
@@ -124,3 +124,54 @@ $(document).ready(function (){
         }
     })
 })
+
+
+// Add to cart functionality
+$(".add-to-cart-btn").on("click", function(){
+
+    let this_val = $(this)
+    let index = this_val.attr("data-index")
+
+    let quantity = $(".product-quantity-" + index).val()
+    let product_title = $(".product-title-" + index).val()
+
+    let product_id = $(".product-id-" + index).val()
+    let product_price = $(".current-product-price-" + index).text()
+
+    let product_pid = $(".product-pid-" + index).val()
+    let product_image = $(".product-image-" + index)
+
+
+
+    console.log("Quantity:", quantity);
+    console.log("Title:", product_title);
+    console.log("Price:", product_price);
+    console.log("ID:", product_id);
+    console.log("PID:", product_pid);
+    console.log("Image:", product_image);
+    console.log("Index", index);
+    console.log("Current Element:", this_val);
+
+    $.ajax({
+        url:'/add-to-cart',
+        data: {
+            'id': product_id,
+            'pid':product_pid,
+            'image': product_image,
+            'qty': quantity,
+            'title': product_title,
+            'price':product_price,
+        },
+        dataType: 'json',
+        beforeSend: function(){
+            console.log("Adding Product to Cart...");
+        },
+        success: function(response){
+            //this_val.html("Tick...lol")
+            this_val.html("<i class='fas fa-check-circle'></i>")
+
+            console.log("Added Product to Cart!");
+            $(".cart-items.count").text(response.totalcartitems)
+        },
+    })
+})
