2c46a1e27c54595dd7b07a4b06c8d8ba153099e6
SichangiFelix <sichangifelix23@gmail.com>
2023-10-27 15:22:53 +0300

    Url for the cart view

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from core.views import index, category_list_view, product_list_view, category_product_list_view, vendor_list_view, vendor_detail_view, product_detail_view, tag_list, ajax_add_review, search_view, filter_product, add_to_cart
+from core.views import index, category_list_view, product_list_view, category_product_list_view, vendor_list_view, vendor_detail_view, product_detail_view, tag_list, ajax_add_review, search_view, filter_product, add_to_cart, cart_view
 
 app_name = "core"
 
@@ -26,6 +26,10 @@ urlpatterns = [
     #Filter
     path("filter-products/", filter_product, name="filter-product"),
 
+    #Add-to cart url
     path("add-to-cart/" , add_to_cart, name="add-to-cart"),
+
+    #cart page url
+    path("cart/", cart_view, name="cart"),
 ]
 
